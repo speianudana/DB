07a931e198af8ebd96c58ec719184416db18b186
speianudana <42315575+speianudana@users.noreply.github.com>
2018-12-08 01:03:46 +0200

    Add files via upload

---
+++ Laboratory_10/Scripts/ex4.sql
@@ -0,0 +1,19 @@
+﻿ DROP TRIGGER IF EXISTS ex4 ON DATABASE;  
+   GO
+CREATE TRIGGER ex4
+on database
+for ALTER_TABLE
+AS
+SET NOCOUNT ON
+DECLARE @Disciplina varchar(50)
+SET @Disciplina =EVENTDATA(). value('(/EVENT_INSTANCE/AlterTableActionList/*/Columns/Name)[1]','nvarchar(max)')
+IF @Disciplina='Disciplina'
+BEGIN
+PRINT ('Coloana Disciplina nu poate fi modificată');
+ROLLBACK;
+END
+go
+
+ALTER TABLE discipline
+ALTER COLUMN Disciplina varchar(50)
+
